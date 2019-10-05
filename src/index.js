bcbd8d1a3e2202fdf870e0790503c3892f311133
Brett Sartain <brettsartain@Bretts-MBP.lan>
2019-10-05 16:49:42 -0400

    Fixed styling for mobile on percentage bubbles. Added click event for video overlay

--- src/index.js
+++ src/index.js
@@ -7,3 +7,20 @@ import 'bootstrap';
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
 });
+
+const wistiaVideoContainer = document.querySelector('#play-button-overlay-zwflowymel')
+const videoOverlay = document.querySelector('.play-button-overlay')
+wistiaVideoContainer.addEventListener('click', function(){
+
+  window._wq = window._wq || [];
+  _wq.push({ id: 'zwflowymel', onReady: function(video) {
+    videoOverlay.setAttribute("style", "z-index: 0");
+    console.log('VIDEO STATE', video.state())
+    if (video.state() === "playing") {
+      video.pause();
+    } else if(video.state() === "pause" || video.state() === "beforePlay") {
+      video.play();
+    }
+  }});
+
+})
